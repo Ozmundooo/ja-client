774b433be955f667eff4fac6f740f0fe38c9de0c
Ozmundooo <osman.ali.dev@gmail.com>
2022-12-19 12:44:54 -0500

    sqFeet split issue

--- src/Components/PropertyCard/PropertyCard.js
+++ src/Components/PropertyCard/PropertyCard.js
@@ -1,24 +1,28 @@
-import { Link } from 'react-router-dom';
-import './PropertyCard.scss';
+import { Link } from "react-router-dom";
+import "./PropertyCard.scss";
 
 function PropertyCard(props) {
   return (
     <Link
       to={
-        props.page === 'featured' ?
-          `/featured/property/${props.link}` :
-          `/property/${props.boardId}/${props.link}`
+        props.page === "featured"
+          ? `/featured/property/${props.link}`
+          : `/property/${props.boardId}/${props.link}`
       }
-      className='propertycard'
+      className="propertycard"
     >
-      <img className='propertycard__img' src={props.image} alt="" />
-      <h4 className='propertycard__title'>{props.title}</h4>
-      <p className='propertycard__text'>${props.text}</p>
-      <p className='propertycard__subtext'>{props.city}</p>
-      <p className='propertycard__subtext'>
-        {props.beds} Beds, {props.baths} Baths{props.sqfeet ? `, ${props.sqfeet.split(' ')[0].split('.')[0]} Sq. Ft.` : '' }{props.built ? `, Built ${props.built}` : ''}
+      <img className="propertycard__img" src={props.image} alt="" />
+      <h4 className="propertycard__title">{props.title}</h4>
+      <p className="propertycard__text">${props.text}</p>
+      <p className="propertycard__subtext">{props.city}</p>
+      <p className="propertycard__subtext">
+        {props.beds} Beds, {props.baths} Baths
+        {/* {props.sqfeet
+          ? `, ${props.sqfeet.split(" ")[0].split(".")[0]} Sq. Ft.`
+          : ""} */}
+        {props.built ? `, Built ${props.built}` : ""}
       </p>
-      <p className='propertycard__subtext'>{props.type}</p>
+      <p className="propertycard__subtext">{props.type}</p>
     </Link>
   );
 }
