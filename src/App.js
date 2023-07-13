ac0f630a12000f28ea52df998317f4df71e9eb2c
Ozmundooo <osman.ali.dev@gmail.com>
2023-07-13 10:36:48 -0400

    reset

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import ScrollToTop from './helpers/ScrollToTop';
-import Home from './Pages/Home/Home';
-import About from './Pages/About/About';
-import Testimonials from './Pages/Testimonials/Testimonials';
-import LocalPicks from './Pages/LocalPicks/LocalPicks';
-import Florida from './Pages/Florida/Florida';
-import Ontario from './Pages/Ontario/Ontario';
-import FeaturedListing from './Pages/FeaturedListing/FeaturedListing';
-import PropertyListing from './Pages/PropertyListing/PropertyListing';
-import Property from './Pages/Property/Property';
-import City from './Pages/City/City';
-import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
-import './partials/_common.scss';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ScrollToTop from "./helpers/ScrollToTop";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Testimonials from "./Pages/Testimonials/Testimonials";
+import LocalPicks from "./Pages/LocalPicks/LocalPicks";
+import Florida from "./Pages/Florida/Florida";
+import Ontario from "./Pages/Ontario/Ontario";
+import FeaturedListing from "./Pages/FeaturedListing/FeaturedListing";
+import PropertyListing from "./Pages/PropertyListing/PropertyListing";
+import Property from "./Pages/Property/Property";
+import City from "./Pages/City/City";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import "./partials/_common.scss";
 
 function App() {
   return (
     <Router>
       <ScrollToTop />
-      <Header />
+      {/* <Header />
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/about' component={About} />
@@ -31,7 +31,7 @@ function App() {
         <Route path='/property/:boardId/:property' component={Property} />
         <Route path='/city/:city' component={City} />
       </Switch>
-      <Footer />
+      <Footer /> */}
     </Router>
   );
 }
